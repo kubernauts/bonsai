ed6caeb81ede35cdbb58f7060c933d5941fc1621
Alexander Giesa <alex@77g.de>
2021-02-18 14:52:10 +0100

    metallb ns deployment added
    Signed-off-by: Alexander Giesa <alex@77g.de>

--- 9-install-metal-lb.sh
+++ 9-install-metal-lb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 export KUBECONFIG=`pwd`/k3s.yaml
+kubectl create ns metallb-system
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create -f metal-lb-layer2-config.yaml
